5188b3b2dec8199e880406bb885b9dcdc36732b7
NgohBuilds <DeepStacker07@gmail.com>
2026-06-18 22:07:16 +0100

    Practice Conditionnal with Raindrop challenge

---
+++ python_Exercise/Day_2_conditionnal/raindrop.py
@@ -0,0 +1,16 @@
+'''
+Lint 
+'''
+def convert(number):
+    """ Function that conert number"""
+    result =""
+    if number % 3 == 0 :
+        result= "Pling"
+    if number % 5 == 0 :
+        result+= "Plang"
+    if number % 7 == 0:
+        result+= "Plong"
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
+        return str(number)
+    return result
+    
