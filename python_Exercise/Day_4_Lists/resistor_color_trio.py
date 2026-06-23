ea100e2fa8384b26ea27cd04d78df06a482cac03
NgohBuilds <DeepStacker07@gmail.com>
2026-06-23 09:25:31 +0100

    Day 4 : resistor color trio challenge -(refactored)

--- python_Exercise/Day_4_Lists/resistor_color_trio.py
+++ python_Exercise/Day_4_Lists/resistor_color_trio.py
@@ -1,14 +1,25 @@
 """ Some text """
-COLORS = {"black": 0,"brown": 1,"red":2,"orange": 3,"yellow": 4,"green": 5,"blue": 6,"violet": 7,"grey": 8,"white": 9}
-EQUIVALENT = {"0":" ohms","3":" kiloohms", "6":" megaohms","9":" gigaohms"}
+
+COLOR_VALUES = {"black": 0,"brown": 1,"red": 2,"orange": 3,"yellow": 4,"green": 5,"blue": 6,"violet": 7,"grey": 8,"white": 9,
+}
+EQUIVALENT = {0:" ohms",3:" kiloohms", 6:" megaohms",9:" gigaohms"}
+
 
 def label(colors):
-    """label function : translating the colors into a label"""
-    two_first_digit = COLORS[colors[0]]*10 + COLORS[colors[1]]
-    number_of_zero = COLORS [colors[2]]
-    if COLORS[colors[1]] == 0:
-        number_of_zero = COLORS [colors[2]] + 1 
-        two_first_digit = COLORS[colors[0]]
-        if COLORS[colors[0]]==0:
-            two_first_digit=""
-    return str(two_first_digit)+ "0"*(number_of_zero % 3)+EQUIVALENT[str(number_of_zero - number_of_zero % 3)]
+    """label function : translate the colors in arguement into a label."""
+    
+    first_digit_value , second_digit_value ,number_of_zero  = [COLOR_VALUES[color] for color in colors[:3]]
+    two_first_digit = first_digit_value * 10 + second_digit_value
+    
+    if two_first_digit == 0 : 
+        return "0 ohms"
+        
+    if two_first_digit  % 10 == 0:  # two_first = 10
+        number_of_zero += 1
+        two_first_digit = first_digit_value
+        
+    remaining_zero = number_of_zero % 3
+    zero_suit = '0'*(remaining_zero)
+
+    return f"{two_first_digit}{zero_suit}{EQUIVALENT[number_of_zero - remaining_zero]}"
+
