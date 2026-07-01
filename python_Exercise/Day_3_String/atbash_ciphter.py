2f2e000311fbfcbf3110c7ebbaafac66ed672d48
NgohBuilds <DeepStacker07@gmail.com>
2026-07-01 19:45:33 +0100

    atbash cipher challenge from exercism.org

--- python_Exercise/Day_3_String/atbash_ciphter.py
+++ python_Exercise/Day_3_String/atbash_ciphter.py
@@ -1,26 +1,47 @@
-PLAIN = "abcdefghijklmnopqrstuvwxyz"
-CIPHER ="zyxwvutsrqponmlkjihgfedcba"
+"""
+Create an implementation of the Atbash cipher, an ancient encryption system created     in the Middle East.
+"""
+import string 
+
+PLAIN = string.ascii_lowercase
+CIPHER =list(reversed(string.ascii_lowercase))
+LOOK_UP = str.maketrans({plain : cipher for plain,cipher in zip(PLAIN,CIPHER)})
+PUNCTUATION = string.punctuation
+
 
 def encode(plain_text):
-    cipher_text = ""
-    max_letter = 5
-    for letter in plain_text.lower():
-        if letter.isalpha():
-            if len(cipher_text) < max_letter :
-                cipher_text+=CIPHER[PLAIN.find(letter)]
-            else:
-                if letter.isalpha():
-                    cipher_text+=" "+CIPHER[PLAIN.find(letter)]
-                else:
-                    cipher_text+=" "+letter
-                max_letter = len(cipher_text) + 4
-        else:
-                if letter.isalpha():
-                    cipher_text+=" "+CIPHER[PLAIN.find(letter)]
-                else:
-                    cipher_text+=" "+letter
-                max_letter = len(cipher_text) + 4
-    return cipher_text
-
+    
+    """    
+        encode (function) : translate latin word/sentence to cipher word/sentence
+          
+        - Args :
+              - plain_text (str) : latin word
+        - Returns:
+              - cipher (str) : cipher word of plain_text
+    """
+    count = 0
+    cipher_string = "".join(plain_text.lower().translate(LOOK_UP).split()) # cipher string but with punctuation 
+    cipher = ""
+      
+    for letter in cipher_string :
+        if letter.isalnum():
+            if count < 5 :
+                cipher += letter
+                count += 1
+            else :
+                cipher += " "+letter
+                count = 1
+                  
+    return cipher
+      
 def decode(ciphered_text):
-    pass
+    """    
+        decode (function) : translate cipher word/sentence to latin word/sentence
+          
+        - Args :
+            - ciphered_text (str) :  ciphered text of a latin text
+        - Returns:
+            - latin text (str) : latin text that match ciphered text
+    """   
+    
+    return "".join(ciphered_text.translate(LOOK_UP).split())
