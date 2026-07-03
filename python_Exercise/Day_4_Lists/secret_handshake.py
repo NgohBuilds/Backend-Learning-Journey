2c997b4608b7a73d30c2957450d95e37da0ccf5f
NgohBuilds <DeepStacker07@gmail.com>
2026-07-03 14:09:59 +0100

    secret handshake challenge from exercism.org (modified)

--- python_Exercise/Day_4_Lists/secret_handshake.py
+++ python_Exercise/Day_4_Lists/secret_handshake.py
@@ -1,2 +1,12 @@
+"""Secret_handshake challenge """
+
+COMMANDS = ["wink", "double blink", "close your eyes", "jump"]
+
 def commands(binary_str):
-    pass
+    """commands : function that returns a list of command according to arg value """
+    handshake = [
+        COMMANDS[bit_index]
+        for bit_index in range(len(COMMANDS)) 
+        if binary_str[-1 - bit_index] == "1"]
+    
+    return handshake[::-1] if binary_str[0] == "1" else handshake 
