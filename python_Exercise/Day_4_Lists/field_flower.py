1508ee96bb71be19de96dc7080fea453fc8b8519
NgohBuilds <DeepStacker07@gmail.com>
2026-07-08 16:00:17 +0100

    Field Flower Challenge from Exercism --Flexing

---
+++ python_Exercise/Day_4_Lists/field_flower.py
@@ -0,0 +1,14 @@
+"""Flower_field challenge"""
+
+def annotate(garden):
+    """Add flower counts to empty squares in a completed Flower Field garden.
+    
+    Args :
+        garden (list of str) : Flower Field garden to complete
+    Returns:
+        list
+    Raises :
+        ValueError : annotate() receives mallformed input
+    """
+   
+    return garden
