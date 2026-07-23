9e421a541433c409ca4f2380a74973a299554e6f
NgohBuilds <DeepStacker07@gmail.com>
2026-07-23 11:43:13 +0100

    Refactoring : Refine logic to make code more clean. [correction of helper functions]

--- python_Exercise/Day_5_dictionnary/forth.py
+++ python_Exercise/Day_5_dictionnary/forth.py
@@ -1,81 +1,112 @@
 """Forth Challenge."""
+
+def is_operation (op):
+    if op not in OP:
+        raise ValueError("undefined operation")
+    return op
+
 def dup(stack):
     """Duplicate element at the top of stack."""
-    
+    if not stack:
+        raise StackUnderflowError("Insufficient number of items in stack")
+        
     stack.append(stack[-1])
-    return stack
+    return stack  
+
 def drop(stack):
     """Pop element at the top of stack."""
+    if not stack:
+        raise StackUnderflowError("Insufficient number of items in stack")
+        
     stack.pop()
     return stack
+
 def swap(stack):
     """Given a stack of n elements, swap elements at the position n-1 and n-2."""
+    if len(stack) < 2:
+        raise StackUnderflowError("Insufficient number of items in stack")
 
-    last = len(stack) - 1
-    before_last = len(stack) - 2
-    tmp = last
-    stack[last] = stack[before_last]
-    stack[before_last] = stack[tmp]
-    
+    stack[-1], stack[-2] = stack[-2], stack[-1]
     return stack
+
 def over(stack):
+    if len(stack) < 2:
+        raise StackUnderflowError("Insufficient number of items in stack")
+   
     elt_before_last = stack[-2]
     stack.append(elt_before_last)
     return stack
+
 def is_digit(string):
     try :
         s = int(string)
         return True
     except ValueError:
         return False
-def divide (a, b):
-    if b == 0:
-        raise ZeroDivisionError("divide by zero")
-    return a // b
-def add(a,b):
-    return a + b
-def minus(a,b):
-    return a - b
-def multiply(a,b):
-    return a * b
-def is_stack_len_right(stack):
-    if len(stack) < 2 :
+
+def divide (stack):
+    if len(stack) < 2:
         raise StackUnderflowError("Insufficient number of items in stack")
-    return False
-    
+        
+    last, before_last = stack.pop(), stack.pop()
+    if last == 0:
+        raise ZeroDivisionError("divide by zero")
+    stack.append(before_last // last)
+    return stack   
+
+def add(stack):
+    if len(stack) < 2:
+        raise StackUnderflowError("Insufficient number of items in stack")
+
+    last, before_last = stack.pop(), stack.pop()
+    stack.append(last + before_last)
+    return stack
+
+def minus(stack):
+    if len(stack) < 2:
+        raise StackUnderflowError("Insufficient number of items in stack")
+
+    last, before_last = stack.pop(), stack.pop()
+    stack.append(before_last - last)
+    return stack
+
+def multiply(stack):
+    if len(stack) < 2:
+        raise StackUnderflowError("Insufficient number of items in stack")
+
+    last, before_last = stack.pop(), stack.pop()
+    stack.append(last * before_last)
+    return stack
+
 OP = {
     "+" : add ,
     "-" : minus,
     "*" : multiply,
-    "/" : divide
+    "/" : divide,
+    "dup" : dup,
+    "drop" : drop,
+    "swap" : swap,
+    "over" : over   
 }
+
 
 class StackUnderflowError(Exception):
     pass
 
 def evaluate(input_data):
     """Simulate an evaluator for a very simple subset of Forth."""
-
-    # Cas 1 : On peut effectuer des operations simples
-    
     stack = []
     for elt in input_data:
-        tokens = elt.split(" ")
+        # Definir la nature de elt :
+        #  Si Mot personnalisé > [ :<name> (built-in op | number| customised_name)]
         
+        #  Si Mot non personnalisé
+        tokens = elt.split(" ")
         for token in tokens:
             if is_digit(token):
                 stack.append(int(token))
             else:
-                # Gere tous les cas avec les operations
-                if not is_stack_len_right(stack):
-                    last = stack.pop()
-                    before_last = stack.pop()
-                    result = OP[token](before_last, last)
-                    stack.append(result)
-                # Ne reste plus qu'a gerer le cas avec les manip de la stack
-                # Ne reste plus qu'a gerer le cas avec les mots definis du user
+                uniformised_token = is_operation(token.lower())
+                stack = OP[uniformised_token](stack) 
                     
     return stack
-            
-                
-    
