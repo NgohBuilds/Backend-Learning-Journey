237abab6080e04cd14434a6b75014e6368afa435
NgohBuilds <DeepStacker07@gmail.com>
2026-08-14 20:53:33 +0100

    Refacto : Add display_expense feature.

--- Project/personnal_expenses_tracker/expenses.py
+++ Project/personnal_expenses_tracker/expenses.py
@@ -8,6 +8,52 @@ def add_expenses(exp_dico):
     expenses = utils.load_expenses(expenses_file)
     expenses.append(exp_dico)
     utils.save_expenses(expenses_file, expenses )
+
+
+
+def display_expense(expense_id = None):
+
+    expenses = utils.load_expenses(expenses_file)
+
+    if expense_id is None :
+        
+        print("================= ALL EXPENSES =================")
+        for expense in expenses:
+            print(
+                f"""
+                |           | EXPENSE - INFO |                |
+                _____________________________________________
+
+                    ID : {expense["id"]}, 
+                    AMOUNT : {expense["amount"]}, 
+                    CATEGORY : {expense["category"]}
+                    DESCRIPTION : {expense["description"]}
+                    DATE : {expense["date"]}
+                _____________________________________________
+
+
+
+                 """)
+    index_expense = utils.search_expense(expense_id, expenses)
+
+
+    print(
+            f"""
+                |           | EXPENSE - INFO |                |
+                _____________________________________________
+
+                    ID : {expenses[index_expense]["id"]}, 
+                    AMOUNT : {expenses[index_expense]["amount"]}, 
+                    CATEGORY : {expenses[index_expense]["category"]}
+                    DESCRIPTION : {expenses[index_expense]["description"]}
+                    DATE : {expenses[index_expense]["date"]}
+                _____________________________________________
+
+
+
+    """)
+
+    
     
 
 
